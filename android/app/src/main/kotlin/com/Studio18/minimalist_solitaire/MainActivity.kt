3efa8b6a7e93046baa4bd4a662ea1f160ac34960
achase89 <a.chase.esteves@gmail.com>
2024-11-29 22:57:19 -0500

    First restore point. App is working, but items are extending past the card layout.

---
+++ android/app/src/main/kotlin/com/Studio18/minimalist_solitaire/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.Studio18.minimalist_solitaire
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/playing_card.dart
@@ -0,0 +1,51 @@
+enum CardSuit {
+  spades,
+  hearts,
+  diamonds,
+  clubs,
+}
+
+enum CardRank {
+  one,
+  two,
+  three,
+  four,
+  five,
+  six,
+  seven,
+  eight,
+  nine,
+  ten,
+  jack,
+  queen,
+  king
+}
+
+enum CardColor {
+  red,
+  black,
+}
+
+// Simple playing card model
+class PlayingCard {
+  CardSuit cardSuit;
+  CardRank cardRank;
+  bool faceUp;
+  bool opened;
+
+  PlayingCard({
+    required this.cardSuit,
+    required this.cardRank,
+    this.faceUp = false,
+    this.opened = false,
+  });
+
+  CardColor get cardColor {
+    if(cardSuit == CardSuit.hearts || cardSuit == CardSuit.diamonds) {
+      return CardColor.red;
+    } else {
+      return CardColor.black;
+    }
+  }
+
+}
